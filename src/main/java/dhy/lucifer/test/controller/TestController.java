86363d67d19f4578ec920bd5925ccfea655a2952
lucifer163 <sunset_lucifer@163.com>
2016-10-31 10:36:21 +0800

    springmvc+mybatis整合

---
+++ src/main/java/dhy/lucifer/test/controller/TestController.java
@@ -0,0 +1,13 @@
+package dhy.lucifer.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/index/index.html")
+	public String index() {
+		return "index";
+	}
+}
